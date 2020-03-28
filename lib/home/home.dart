1ad343d01a15f0eb84ee9d73cd8569f35d7008e0
naveed rehman <naveedrehman90001@gmail.com>
2020-03-28 12:40:43 +0500

    update

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -8,12 +8,56 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("To-Do-List"),
-      ),
-      body: Center(
-        child: Text("Center"),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        // appBar: AppBar(
+        //   title: Text("To-Do-List"),
+        // ),
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 40, bottom: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 30,
+                      child: Icon(
+                        Icons.menu,
+                        size: 30,
+                      )),
+                  SizedBox(height: 10.0),
+                  Text(
+                    "TO-DO-List",
+                    style: TextStyle(fontSize: 25, color: Colors.white),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    "12 Tasks",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                // height: 300,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      topRight: Radius.circular(10.0)),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
